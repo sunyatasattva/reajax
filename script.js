16be8d253567ef606bfc0e254f69b98da2e6f6e4
prafulla <prafulla@sapnasolutions.com>
2014-10-22 18:45:12 +0530

    in progress menu panel. closes #1344.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Place your js files under javascripts folder.');
+Remix.$(document).on('click', 'header .r-menu', function(e){
+	e.preventDefault();
+	if(Remix.$('#r-menuPanel').css('left') == '-250px'){
+		Remix.$('#r-menuPanel').css('left','0px');
+		Remix.$('#r-menuPanel').prev('form').css('left','250px');
+	}else{
+		Remix.$('#r-menuPanel').css('left','-250px');
+		Remix.$('#r-menuPanel').prev('form').css('left','0px');
+	}
+})
