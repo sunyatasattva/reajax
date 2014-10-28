5f3fb0fddda2e2de571767a8731d77bca57c71a6
Robert <robert@sapnasolutions.com>
2014-10-28 14:23:26 +0530

    Completes Offerte detail page. Completes #1348

--- script.js
+++ script.js
@@ -13,14 +13,18 @@ Remix.$(document).on('click', 'header .r-menu', function(e){
 /*for accordian*/
 Remix.$(document).on('click', '.r-collapsHeading', function(e){
 	e.preventDefault();
+	Remix.$('.r-accordianHeading').removeClass("r-active");
 	Remix.$(this).closest('.r-accordian').children().children('.r-accordianHeading').find('.r-icon').addClass('icon-angle-right').removeClass('icon-angle-up');
+	Remix.$(this).closest('.r-accordian').children().children('.r-accordianHeading').find('.r-iconContent').addClass('icon-angle-down').removeClass('icon-angle-up');
 	if(Remix.$(this).parent().next().css('display') == 'none'){
 		Remix.$(this).closest('.r-accordian').children().children('.r-accordianContent').hide();
-		Remix.$(this).closest('.r-accordianHeading').next().show();
+		Remix.$(this).closest('.r-accordianHeading').addClass("r-active").next().show();
 		Remix.$(this).children('.r-icon').addClass('icon-angle-up').removeClass('icon-angle-right');
+		Remix.$(this).children('.r-iconContent').addClass('icon-angle-up').removeClass('icon-angle-down');
 	}else{
 		Remix.$(this).closest('.r-accordian').find('.r-accordianContent').hide();
 		Remix.$(this).children('.r-icon').addClass('icon-angle-right').removeClass('icon-angle-up');
+		Remix.$(this).children('.r-iconContent').addClass('icon-angle-down').removeClass('icon-angle-up');
 	}
 });
 
